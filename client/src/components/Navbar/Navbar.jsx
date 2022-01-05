db1db9f02d08dcc920ca25b385f0052f4a7aa7e2
hossain-shahrier <hossainshahrier.shm@gmail.com>
2022-01-05 23:39:37 +0600

    Navbar component started

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+// Icons
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+// Logo
+import logo from "../../../images/logo.png";
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4 ">
+
+      <div className="md:flex-[0.5] flex-initial"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
